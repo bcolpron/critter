b5aba8f38006e3414295977706ffe6069db0ce17
Bertin Colpron <bertin.colpron@kontron.com>
2018-10-20 14:23:36 -0400

    refactory registry in its own file

--- include/httpserver/httpserver.h
+++ include/httpserver/httpserver.h
@@ -2,8 +2,7 @@
 
 #define BOOST_COROUTINES_NO_DEPRECATION_WARNING
 
-#include <boost/beast/core.hpp>
-#include <boost/beast/http.hpp>
+#include "registry.h"
 #include <boost/beast/version.hpp>
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/asio/spawn.hpp>
@@ -15,50 +14,8 @@
 #include <string>
 #include <thread>
 #include <vector>
-#include <regex>
 
 namespace http = boost::beast::http;
-
-namespace detail
-{
-
-class Registry
-{
-    using Handler = std::function<http::response<http::string_body>(http::request<http::string_body>)>;
-    using Entry = std::tuple<http::verb, std::regex, Handler>;
-public:
-
-    struct NotFound: std::out_of_range {
-        NotFound(): std::out_of_range("not found") {}
-    };
-
-    template<class F>
-    void add(http::verb v, boost::beast::string_view uri, F&& f)
-    {
-        resource_table.emplace_back(std::move(v), std::regex(uri.begin(), uri.end()), Handler(f));
-    }
-
-    const Handler& get(http::verb verb, boost::beast::string_view uri) const
-    {
-        auto pred = [&](const auto& entry){
-            std::cmatch match;
-            return verb == std::get<0>(entry) 
-                && std::regex_match(uri.begin(), uri.end(), match, std::get<1>(entry));
-        };
-        auto found = 
-            std::find_if(begin(resource_table), end(resource_table), pred);
-        if (found != resource_table.end())
-        {
-            return std::get<2>(*found);
-        }
-        throw NotFound();
-    }
-
-private:
-    std::vector<Entry> resource_table;
-};
-
-}
 
 http::response<http::string_body> make_response(
     http::request<http::string_body> request,
---
+++ include/httpserver/registry.h
@@ -0,0 +1,49 @@
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <stdexcept>
+#include <vector>
+#include <tuple>
+#include <regex>
+
+namespace http = boost::beast::http;
+
+namespace detail
+{
+
+class Registry
+{
+    using Handler = std::function<http::response<http::string_body>(http::request<http::string_body>)>;
+    using Entry = std::tuple<http::verb, std::regex, Handler>;
+public:
+
+    struct NotFound: std::out_of_range {
+        NotFound(): std::out_of_range("not found") {}
+    };
+
+    template<class F>
+    void add(http::verb v, boost::beast::string_view uri, F&& f)
+    {
+        resource_table.emplace_back(std::move(v), std::regex(uri.begin(), uri.end()), Handler(f));
+    }
+
+    const Handler& get(http::verb verb, boost::beast::string_view uri) const
+    {
+        auto pred = [&](const auto& entry){
+            std::cmatch match;
+            return verb == std::get<0>(entry) 
+                && std::regex_match(uri.begin(), uri.end(), match, std::get<1>(entry));
+        };
+        auto found = 
+            std::find_if(begin(resource_table), end(resource_table), pred);
+        if (found != resource_table.end())
+        {
+            return std::get<2>(*found);
+        }
+        throw NotFound();
+    }
+
+private:
+    std::vector<Entry> resource_table;
+};
+
+}
