a77d0e7369ef3d81c31354b334b487e35f5866cc
Bertin Colpron <bertin.colpron@kontron.com>
2019-01-21 15:16:08 -0500

    sample app

---
+++ example/main.cpp
@@ -0,0 +1,16 @@
+#include "httpserver/httpserver.h"
+
+int main(int, const char**)
+{
+    HttpServer server(8888);
+    server.serve_files("/static", ".");
+    server.add_http_handler(http::verb::get, "/test/?", [](http::request<http::string_body>&& req) -> http::response<http::string_body>
+    {
+        return make_response(req, "Hello\n");
+    });
+    server.add_ws_handler("/ws");
+
+    std::cout << "Server started" << std::endl; 
+    server.run();
+    return 0;
+}
