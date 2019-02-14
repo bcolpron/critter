7b50a64b09208574488663f5e2dd35b389befbef
Bertin Colpron <bcolpron78@gmail.com>
2019-02-14 13:58:13 -0500

    ws socket state callback

--- example/www/ServerConnection.js
+++ example/www/ServerConnection.js
@@ -1,7 +1,7 @@
 function ServerConnection(url) {
     this.url = url;
     this.closing = false;
-    this._onConnectCallback = function() {};
+    this._onStateChangeCallback = function() {};
     this._onMessageCallback = function(msg) {console.log("message from server: " + msg);};
     this.connect();
 }
@@ -9,6 +9,7 @@ function ServerConnection(url) {
 ServerConnection.prototype.connect = function() {
     this.ws = new WebSocket(this.url);
     console.log("Connecting...");
+    this._onStateChangeCallback(this.ws.readyState);
 
     this.ws.onopen = $.proxy(this._onopen, this);
     this.ws.onclose = $.proxy(this._onclose, this);
@@ -18,18 +19,16 @@ ServerConnection.prototype.connect = function() {
 
 ServerConnection.prototype._onopen = function() {
     console.log("Connected!")
-    this._onConnectCallback();
+    this._onStateChangeCallback(this.ws.readyState);
 }
 
 ServerConnection.prototype._onmessage = function(event) {
     this._onMessageCallback(event.data);
 }
 
-ServerConnection.prototype.onConnect = function(callback) {
-    this._onConnectCallback = callback;
-    if (this.ws && this.ws.readyState === this.ws.OPEN) {
-        callback();
-    }
+ServerConnection.prototype.onStateChange = function(callback) {
+    this._onStateChangeCallback = callback;
+    this._onStateChangeCallback(this.ws.readyState);
 }
 
 ServerConnection.prototype.onMessage = function(callback) {
@@ -48,6 +47,7 @@ ServerConnection.prototype.send = function(data) {
 
 ServerConnection.prototype._onclose = function() {
     if (this.closing) return;
+    this._onStateChangeCallback(this.ws.readyState);
     console.log("connection closed. Reconnecting...")
     setTimeout($.proxy(function() {
         this.connect();
