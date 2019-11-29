bd73adcba4236548f3f7a95b6642f5740c61e9fc
Bertin Colpron <bertin.colpron@kontron.com>
2019-11-29 12:18:56 -0500

    Add a POST handler example

--- example/main.cpp
+++ example/main.cpp
@@ -8,6 +8,11 @@ int main(int, const char**)
     {
         return make_response(req, "Hello\n");
     });
+    server.add_http_handler(http::verb::post, "/test/?", [](auto&& req)
+    {
+        std::cout << req.body() << std::endl;
+        return make_response(req, "ok\n");
+    });
     server.add_ws_handler("/ws(/.*)?", [&](auto msg, auto& session) {
         std::cout << msg << std::endl;
         // echo the message to all clients
